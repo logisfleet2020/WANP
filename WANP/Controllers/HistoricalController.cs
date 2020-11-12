1aa149cb5028d90b87f4b8aaa62961de7104e32c
logisfleet2020 <logisfleet2020@outlook.com>
2020-11-12 17:26:32 +0800

    two request and result inside one try block

--- WANP/Controllers/HistoricalController.cs
+++ WANP/Controllers/HistoricalController.cs
@@ -8,6 +8,7 @@ using WANP.Models;
 using RestSharp;
 using RestSharp.Authenticators;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace WANP.Controllers
 {
@@ -36,6 +37,11 @@ namespace WANP.Controllers
             {
                 var client = new RestClient("https://fms.logisfleet.com/comGpsGate/api/v.1/applications");
                 //client.AddDefaultParameter("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+
+                var userIdCarPlateRequest = new RestRequest("252/users");
+                userIdCarPlateRequest.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+
+                var usersResult = await client.GetAsync<List<UsersResponseModel>>(userIdCarPlateRequest);
 
                 var request = new RestRequest("/252/users/1250/tracks?Date=2020-10-20");
                 request.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
---
+++ WANP/Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WANP.Models
+{
+    public class ResponseModel
+    {
+        public string CarPlateNo { get; set; }
+        public string TimeStamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
