daacf5a0203d6a3357203507160a1498e32174a3
logisfleet2020 <logisfleet2020@outlook.com>
2020-11-13 13:47:56 +0800

    Add in check for invalid car plate not inside the application.

--- WANP/Controllers/HistoricalController.cs
+++ WANP/Controllers/HistoricalController.cs
@@ -30,6 +30,13 @@ namespace WANP.Controllers
             return "value";
         }
 
+        public string formatUTCDate(DateTime dateFromDb)
+        {
+            var localDateTime = dateFromDb.ToLocalTime();
+            var formattedStr = String.Format("{0: yyyy-MM-dd HH:mm:ss}",localDateTime);
+            return formattedStr;
+        }
+
 
         [Route("api/Historical")]
         public async Task<IHttpActionResult> GetAsync([FromBody] RequestModel model){
@@ -38,6 +45,7 @@ namespace WANP.Controllers
             {
 
                 var tracksResult = new List<TrackModel>();
+                var formattedResponse = new List<ResponseTrackModel>();
                 var firstResult = new List<TrackModel>();
                 var secondResult = new List<TrackModel>();
 
@@ -51,48 +59,63 @@ namespace WANP.Controllers
 
                 var carPlateMatch = usersResult.Where(user => user.name == requestedCarPlate);
 
-                var idToRequest = carPlateMatch.ToList()[0].id;
+                if (carPlateMatch.Count() == 1)
+                {
 
-                if (model.FromDate == model.ToDate) {
-                    if (model.FromTime != null && model.ToDate != null && model.ToTime != null)
+                    var idToRequest = carPlateMatch.ToList()[0].id;
+
+                    if (model.FromDate == model.ToDate)
                     {
-                        var requestWithAllParameters = new RestRequest("/252/users/" + idToRequest + "/tracks?Date="+model.FromDate+"&From=" + model.FromTime + "&Until=" + model.ToTime);
-                        requestWithAllParameters.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+                        if (model.FromTime != null && model.ToDate != null && model.ToTime != null)
+                        {
+                            var requestWithAllParameters = new RestRequest("/252/users/" + idToRequest + "/tracks?Date=" + model.FromDate + "&From=" + model.FromTime + "&Until=" + model.ToTime);
+                            requestWithAllParameters.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
 
-                         tracksResult = await client.GetAsync<List<TrackModel>>(requestWithAllParameters);
+                            tracksResult = await client.GetAsync<List<TrackModel>>(requestWithAllParameters);
 
-                        var newList = tracksResult.Select(track => { track.utc = track.utc.ToLocalTime(); return track; }).ToList();
-                         return Json(newList);
+                            for (int i = 0; i < tracksResult.Count; i++)
+                            {
+                                formattedResponse.Add(new ResponseTrackModel { CarPlateNo = model.CarPlateNo, TimeStamp = formatUTCDate(tracksResult[i].utc), Latitude = tracksResult[i].position.latitude, Longitude = tracksResult[i].position.longitude });
+                            }
+
+                            //var newList = tracksResult.Select(track => { track.utc = formatUTCDate(track.utc); return track; }).ToList();
+                            return Json(formattedResponse);
+                        }
                     }
+                    else
+                    {
+                        var firstRequest = new RestRequest("/252/users/" + idToRequest + "/tracks?Date=" + model.FromDate + "&From=" + model.FromTime + "&Until=23:59:59");
+                        firstRequest.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+
+                        firstResult = await client.GetAsync<List<TrackModel>>(firstRequest);
+
+
+
+                        var secondRequest = new RestRequest("/252/users/" + idToRequest + "/tracks?Date=" + model.ToDate + "&From=00:00:00&Until=" + model.ToTime);
+                        secondRequest.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+
+                        secondResult = await client.GetAsync<List<TrackModel>>(secondRequest);
+
+                        firstResult.AddRange(secondResult);
+
+                        return Json(firstResult);
+
+
+
+                    }
+
+                    /*
+                    var request = new RestRequest("/252/users/"+idToRequest+"/tracks?Date=2020-10-20");
+                    request.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
+
+                    var result = await client.GetAsync<List<TrackModel>>(request);
+                    */
+                    //end try here. 
                 }
                 else
                 {
-                    var firstRequest = new RestRequest("/252/users/" + idToRequest + "/tracks?Date=" + model.FromDate + "&From=" + model.FromTime + "&Until=23:59:59");
-                    firstRequest.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
-
-                     firstResult = await client.GetAsync<List<TrackModel>>(firstRequest);
-
-
-
-                    var secondRequest = new RestRequest("/252/users/" + idToRequest + "/tracks?Date=" + model.ToDate + "&From=00:00:00&Until=" + model.ToTime);
-                    secondRequest.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
-
-                     secondResult = await client.GetAsync<List<TrackModel>>(secondRequest);
-
-                    firstResult.AddRange(secondResult);
-
-                    return Json(firstResult);
-
-                    
-
+                    return Json("Not a valid car plate");
                 }
-
-                /*
-                var request = new RestRequest("/252/users/"+idToRequest+"/tracks?Date=2020-10-20");
-                request.AddHeader("Authorization", "YhH6C5FlWp0EYlcKXCcQdzCBmaEUxoXf8AFLfEI%2fh2zu7DE%2biS%2f42L4j25Gc43H%2b");
-
-                var result = await client.GetAsync<List<TrackModel>>(request);
-                */
             }catch(Exception e)
             {
                 Console.Write(e);
---
+++ WANP/Models/ResponseTrackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WANP.Models
+{
+    public class ResponseTrackModel
+    {
+        public string CarPlateNo { get; set; }
+        public string TimeStamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
